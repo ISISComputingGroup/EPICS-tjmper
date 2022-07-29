6151f1b0536ecec626d35ef24ba7326f2367f3f4
Nikola Roev <nikola.roev@stfc.ac.uk>
2022-07-29 10:48:36 +0100

    Extended tests.

--- system_tests/tests/tjmper.py
+++ system_tests/tests/tjmper.py
@@ -6,7 +6,7 @@ from parameterized import parameterized
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim, parameterized_list
+from utils.testing import get_running_lewis_and_ioc, parameterized_list
 
 
 DEVICE_PREFIX = "TJMPER_01"
@@ -32,6 +32,18 @@ MODES = [
     "PLT2 and SMPL engaged"
 ]
 
+LIMITS = [
+    [1, 0, 1, 0, 1, 0],
+    [0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 1],
+    [0, 1, 1, 0, 1, 0],
+    [1, 0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 1, 0],
+    [1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0]
+]
+
 AIR_SUPPLY = [
     (0, "Off Dumped"),
     (1, "Off Valve Reset"),
@@ -40,15 +52,15 @@ AIR_SUPPLY = [
 ]
 
 ERRORS = [
-    (0, "No Error (Operational)"),
-    (1, "PLT1 Homing TMO"),
-    (2, "PLT1 Engaging TMO"),
-    (3, "PLT1EN SMPL Homing TMO"),
-    (4, "PLT1EN SMPL Engaging TMO"),
-    (5, "PLT2 Homing TMO"),
-    (6, "PLT2 Engaging TMO"),
-    (7, "PLT2EN SMPL Homing TMO"),
-    (8, "PLT2EN SMPL Engaging TMO")
+    (0, "No Error (Operational)",   ChannelAccess.Alarms.NONE),
+    (1, "PLT1 Homing TMO",          ChannelAccess.Alarms.MAJOR),
+    (2, "PLT1 Engaging TMO",        ChannelAccess.Alarms.MAJOR),
+    (3, "PLT1EN SMPL Homing TMO",   ChannelAccess.Alarms.MAJOR),
+    (4, "PLT1EN SMPL Engaging TMO", ChannelAccess.Alarms.MAJOR),
+    (5, "PLT2 Homing TMO",          ChannelAccess.Alarms.MAJOR),
+    (6, "PLT2 Engaging TMO",        ChannelAccess.Alarms.MAJOR),
+    (7, "PLT2EN SMPL Homing TMO",   ChannelAccess.Alarms.MAJOR),
+    (8, "PLT2EN SMPL Engaging TMO", ChannelAccess.Alarms.MAJOR)
 ]
 
 ALARM_PVS = [
@@ -80,29 +92,39 @@ class TjmperTests(unittest.TestCase):
         self._lewis.backdoor_set_on_device("id", id_value)
         self.ca.assert_that_pv_is("ID", id_value)
 
+    @parameterized.expand(parameterized_list(LIMITS))
+    def test_WHEN_limits_set_via_backdoor_THEN_limits_update(self, _, states):
+        self._lewis.backdoor_set_on_device("plate_1_home", states[0])
+        self._lewis.backdoor_set_on_device("plate_1_engaged", states[1])
+        self._lewis.backdoor_set_on_device("plate_2_home", states[2])
+        self._lewis.backdoor_set_on_device("plate_2_engaged", states[3])
+        self._lewis.backdoor_set_on_device("sample_home", states[4])
+        self._lewis.backdoor_set_on_device("sample_engaged", states[5])
+
+        convert = lambda n: "True" if n == 1 else "False"
+        self.ca.assert_that_pv_is("LMT:PLATE1:HOME", convert(states[0]))
+        self.ca.assert_that_pv_is("LMT:PLATE1:ENGAGED", convert(states[1]))
+        self.ca.assert_that_pv_is("LMT:PLATE2:HOME", convert(states[2]))
+        self.ca.assert_that_pv_is("LMT:PLATE2:ENGAGED", convert(states[3]))
+        self.ca.assert_that_pv_is("LMT:SAMPLE:HOME", convert(states[4]))
+        self.ca.assert_that_pv_is("LMT:SAMPLE:ENGAGED", convert(states[5]))
+        
     @parameterized.expand(parameterized_list(AIR_SUPPLY))
     def test_WHEN_air_supply_set_via_backdoor_THEN_air_supply_updates(self, _, code, string):
         self._lewis.backdoor_set_on_device("air_supply", code)
         self.ca.assert_that_pv_is("AIR", string)
 
     @parameterized.expand(parameterized_list(ERRORS))
-    def test_WHEN_error_set_via_backdoor_THEN_error_updates(self, _, code, string):
+    def test_WHEN_error_set_via_backdoor_THEN_error_updates(self, _, code, string, state):
         self._lewis.backdoor_set_on_device("error_state", code)
         self.ca.assert_that_pv_is("ERR", string)
+        self.ca.assert_that_pv_alarm_is("ERR", state)
 
     @parameterized.expand(parameterized_list(MODES))
     def test_WHEN_mode_set_THEN_mode_updates(self, _, mode):
         self.ca.set_pv_value("MODE:SP", mode)
         self.ca.assert_that_pv_is("MODE", mode)
 
-
-        #self.ca.assert_that_pv_is("LMT:PLATE1:HOME", state[0])
-        #self.ca.assert_that_pv_is("LMT:PLATE1:ENGAGED", state[1])
-        #self.ca.assert_that_pv_is("LMT:PLATE2:HOME", state[2])
-        #self.ca.assert_that_pv_is("LMT:PLATE2:ENGAGED", state[3])
-        #self.ca.assert_that_pv_is("LMT:SAMPLE:HOME", state[4])
-        #self.ca.assert_that_pv_is("LMT:SAMPLE:ENGAGED", state[5])
-        
     @contextlib.contextmanager
     def _disconnect_device(self):
         self._lewis.backdoor_set_on_device("connected", False)
